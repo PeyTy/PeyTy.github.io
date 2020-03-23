b91ddfb8252c76f240fda25da622519b71cf53e2
Miraculous Ladybugreport <3642643+PeyTy@users.noreply.github.com>
2020-03-23 08:35:00 -0700

    Build

---
+++ peyty.github.io/src/ssr.mjs
@@ -0,0 +1,6 @@
+import path from 'path'
+import fs from 'fs'
+
+import React from 'react'
+import { renderToString } from 'react-dom/server'
+
---
+++ precache-manifest.6800759548f90db9a4f95d2f0e3b1b6c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0e9676b6aa3ba7bdcb53e8f40062179",
+    "url": "./index.html"
+  },
+  {
+    "revision": "57570823af6b61a6889f",
+    "url": "./static/css/main.5503d0d8.chunk.css"
+  },
+  {
+    "revision": "0b2e205b6f7f60e7568f",
+    "url": "./static/js/2.984b2085.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.984b2085.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57570823af6b61a6889f",
+    "url": "./static/js/main.94193d9b.chunk.js"
+  },
+  {
+    "revision": "f9c13e43f378fe7c9fcd",
+    "url": "./static/js/runtime-main.5d1a1f11.js"
+  },
+  {
+    "revision": "da2aca1f17e1353292116b18c514eb0b",
+    "url": "./static/media/top.da2aca1f.svg"
+  }
+]);
